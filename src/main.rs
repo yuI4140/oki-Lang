2c77d8e655bb5a70bc87f2394fa1947f88d426d9
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-07-02 03:28:22 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![allow(unused)] // For beginning only.
+
+use crate::prelude::*;
+use std::fs::read_dir;
+
+mod error;
+mod prelude;
+mod utils;
+
+fn main() -> Result<()> {
+	println!("Hello, world!");
+	Ok(())
+}
