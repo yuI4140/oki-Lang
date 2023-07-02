0cbaa618d9cc54fd5ba568630a18cb6e717714a1
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-07-02 06:26:08 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,24 @@ use crate::prelude::*;
 mod error;
 mod prelude;
 mod utils;
+use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Type {
-	I32,
-	F64,
-	Str,
-	Var,
-	Null,
-	Operator(Operators),
+    I32,
+    F64,
+    Str,
+    Var,
+    Null,
+    Operator(Operators),
 }
 
-#[derive(Debug)]
+impl Default for Type {
+    fn default() -> Self {
+        Type::Null
+    }
+}
+#[derive(Debug,PartialEq, Clone, Copy)]
 enum Operators {
 	Add,
 	Sub,
@@ -22,13 +28,15 @@ enum Operators {
 	Div,
 	Eq,
 }
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone, Default)]
 struct Token {
 	_type: Type,
 	_value: String,
 }
-
+fn string_to_str(s: String) -> &'static str {
+	let boxed_str: Box<str> = s.into_boxed_str();
+	Box::leak(boxed_str)
+}
 impl Token {
 	fn new(_type: Type, _value: String) -> Option<Token> {
 		match _type {
@@ -117,10 +125,65 @@ fn get_operator(ch: char) -> Operators {
 		_ => panic!("Invalid operator!"),
 	}
 }
+#[derive(Debug, Clone, Default)]
+struct Ast {
+	root: Option<Node>,
+	right: Option<Box<Node>>,
+	left: Option<Box<Node>>,
+}
+impl Ast {
+	fn new(tkl: &(Token, Token, Token)) -> Option<Ast> {
+		let root = Node::new(tkl.0.clone())?;
+		let right = Box::new(Node::new(tkl.1.clone())?);
+		let left = Box::new(Node::new(tkl.2.clone())?);
+		Some(Ast {
+			root: Some(root),
+			right: Some(right),
+			left: Some(left),
+		})
+	}
+}
+#[derive(Debug, Clone)]
+struct Node {
+	token: Token,
+}
+impl Node {
+	fn new(tk: Token) -> Option<Node> {
+		match tk._type {
+			Type::Null => None,
+			_ => Some(Node { token: tk }),
+		}
+	}
+}
+fn convert<T: FromStr<Err = Error>>(string: &str) -> Result<T> {
+	string.parse::<T>()
+}
+fn get_cloned_value<T: Clone + Default>(option: Option<T>) -> T {
+    match option.as_ref() {
+        Some(value) => value.clone(),
+        None => T::default(),
+    }
+}
+fn parser_op(ast: &Ast) -> i32 {
+	if let (Some(root), Some(right), Some(left)) =
+		(ast.root.as_ref(), ast.right.as_ref(), ast.left.as_ref())
+	{
+		let _t = Token::new(Type::I32, String::from(" "));
+		let a = root.token.clone();
+		let op = right.token.clone();
+		let b = left.token.clone();
+		if (a._type != get_cloned_value(_t.clone())._type && b._type != get_cloned_value(_t.clone())._type) {
+			return 0;
+		}
+	}
+	return 0;
+}
 fn main() -> Result<()> {
 	println!("is running!");
-	let input = "var=\"Hello\"";
+
+	let input = "var=2+2";
 	let tokens = tokenize(input);
-	println!("Tokens: {:?}\n", tokens[2]._type);
+	let ast = Ast::new(&(tokens[2].clone(), tokens[3].clone(), tokens[4].clone()));
+	parser_op(&get_cloned_value(ast));
 	Ok(())
 }
